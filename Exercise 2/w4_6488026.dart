d2376f26c78bb5c68119a93418ac4760c9c70d0b
Netnetithorn <149154079+Netnetithorn@users.noreply.github.com>
2024-02-05 14:31:06 +0700

    Update w4_6488026.dart

--- Exercise 2/w4_6488026.dart
+++ Exercise 2/w4_6488026.dart
@@ -1,1 +1,35 @@
+Widget weatherBanner(BuildContext context) {
+  double screenWidth = MediaQuery.of(context).size.width;
 
+  return SizedBox(
+    width: screenWidth - 48,
+    height: 100,
+    child: Row (
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        _weatherBox(
+          screenWidth / 3 - 16,
+          100,
+          customChild: Icon(
+            todayWeather.condition,
+            size: 52,
+          ),
+        ),
+        _weatherBox(
+          screenWidth / 3 - 16,
+          100,
+          title: 'Bangkok',
+          subtitle1: '25°C',
+        ),
+        _weatherBox(
+          screenWidth / 3 - 16,
+          100,
+          title: "Sunny",
+          subtitle1: "Humidity: ${todayWeather.humidity.toString()}%",
+          subtitle2: "Precip: ${todayWeather.precip.toString()}%",
+        ),
+      ),
+    ),
+  );
+}
